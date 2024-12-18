72d8b8c37c89d41619c49ec1f32677ff32c16dd9
mscsgrad123 <projectscs.sjsu@gmail.com>
2024-12-18 00:50:18 -0800

    Updated Constants.js

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -4,7 +4,7 @@ export const oauthConfig = {
   clientId: process.env.REACT_APP_CLIENT_ID, // Replace with your Client ID
   authorizationEndpoint: "https://accounts.google.com/o/oauth2/auth",
   tokenEndpoint: "https://oauth2.googleapis.com/token",
-  redirectUri: "http://localhost:3000",
+  redirectUri: "https://b1.d1l0vpr8rliqum.amplifyapp.com",
   scope: "openid email profile",
   clientSecret: process.env.REACT_APP_CLIENT_SECRET,
 };
